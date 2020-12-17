7210c8d13951520bce4ceddd346db73ef0002c99
Ali Haider Shajeeb <75715728+shajeebHaider@users.noreply.github.com>
2020-12-17 16:31:05 +0600

    Add CategoryModel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Categories
 
-# Register your models here.
+
+
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = [
+        'Catagories_photo',
+        'name',
+        'slug',
+        'description',
+        
+        'isactive'
+    ]
+    prepopulated_fields = {'slug': ("name",)}
+    
+
+admin.site.register(Categories,CategoriesAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
+from django.shortcuts import reverse
+from django.utils.safestring import mark_safe
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length= 100)
+    slug= models.SlugField()
+    description = models.TextField()
+    image = models.ImageField(upload_to = 'Photos')
+    isactive= models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("core:categories", kwargs={'slug': self.slug})    
+    
+    def Catagories_photo(self):
+        return mark_safe('<img src="{}" width="70" height ="70" />'.format(self.image.url))
+    Catagories_photo.short_description = 'Image'
+    Catagories_photo.allow_tags = True
+
+class Brands(models.Model):
+    name = models.CharField(max_length= 100)
+    slug= models.SlugField()
+    description = models.TextField()
+    image = models.ImageField(upload_to = 'Photos')
+    isactive= models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("core:brands", kwargs={'slug': self.slug})    
+    
+    def brands_photo(self):
+        return mark_safe('<img src="{}" width="70" height ="70" />'.format(self.image.url))
+    brands_photo.short_description = 'Image'
+    brands_photo.allow_tags = True
