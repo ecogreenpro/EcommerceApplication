10968c3cf6d959e564ec57cdb992bfeec39f8d31
ecogreenpro <ecogreenproglobal1@gmail.com>
2020-12-27 14:05:24 +0600

    Login Complete

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, View
@@ -51,6 +51,17 @@ def paymentProcess(request):
 
 def login(request):
     context = {}
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return render(request, 'account/userprofile.html')
+        else:
+            messages.info(request, 'Invalid Login')
+    else:
+        return render(request, 'account/login.html', context)
     return render(request, 'account/login.html', context)
 
 
