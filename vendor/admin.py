e402b500e1e639a8888dc794a1675975eeef16a3
Ali Haider Shajeeb <75715728+shajeebHaider@users.noreply.github.com>
2021-01-04 14:55:33 +0600

    Seller Registration

--- vendor/admin.py
+++ vendor/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import SellerRegistration
+
 
 # Register your models here.
+class SellerRegistrationAdmin(admin.ModelAdmin):
+    list_display = [
+        'Name',
+        'CompanyName',
+        'Phone',
+        'NID',
+        'TradeLicense'
+    ]
+
+    list_filter = ['Name']
+    search_fields = ['CompanyName']
+
+
+admin.site.register(SellerRegistration, SellerRegistrationAdmin)
+
--- vendor/models.py
+++ vendor/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class SellerRegistration(models.Model):
+    Name = models.CharField(max_length=50)
+    CompanyName = models.CharField(max_length=50, unique=True, verbose_name='Company Name')
+    Phone = models.CharField(max_length=15)
+    Email = models.EmailField()
+    Address = models.TextField()
+    NID = models.CharField(max_length=50)
+    TradeLicense = models.CharField(max_length=50, verbose_name='Trade License')
+    NIDImage = models.ImageField(upload_to='Photos', verbose_name='NID Image')
+    TradeImage = models.ImageField(upload_to='Photos', verbose_name='Trade License Image')
+
+    def __str__(self):
+        return self.CompanyName
